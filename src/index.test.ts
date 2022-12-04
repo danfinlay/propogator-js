c049dfc8bc82503e0477d22795b66dced2aa7ede
Dan Finlay <dan@danfinlay.com>
2022-12-04 15:28:46 -0800

    Moar test

--- src/index.test.ts
+++ src/index.test.ts
@@ -11,10 +11,39 @@ describe('Cell', () => {
   it('can have a list of values', async () => {
     const colors = ['red', 'blue', 'green'];
     const cell = createCell({ oneOf: colors });
-    const { oneOf } = await cell.getValue();
+    let { oneOf } = await cell.getValue();
     console.dir(oneOf);
     expect(oneOf).toContain(colors[0]);
     expect(oneOf).toContain(colors[1]);
     expect(oneOf).toContain(colors[2]);
+
+    await cell.update({ oneOf: ['red', 'blue' ]});
+    ({ oneOf } = await cell.getValue());
+    expect(oneOf).toContain(colors[0]);
+    expect(oneOf).toContain(colors[1]);
+    expect(oneOf && oneOf.length).toBe(2);
+  });
+
+  it('can have process not values', async () => {
+    const colors = ['red', 'blue', 'green'];
+    const cell = createCell({ oneOf: colors });
+    let { oneOf } = await cell.getValue();
+    console.dir(oneOf);
+    expect(oneOf).toContain(colors[0]);
+    expect(oneOf).toContain(colors[1]);
+    expect(oneOf).toContain(colors[2]);
+
+    await cell.update({ oneOf: ['red', 'blue' ]});
+    ({ oneOf } = await cell.getValue());
+    expect(oneOf).toContain(colors[0]);
+    expect(oneOf).toContain(colors[1]);
+    expect(oneOf && oneOf.length).toBe(2);
+
+    await cell.update({ isNot: ['red']});
+    ({ oneOf } = await cell.getValue());
+    expect(oneOf).toContain(colors[1]);
+    expect(oneOf && oneOf.length).toBe(1);
+    const { value } = await cell.getValue();
+    expect(value).toBe('blue');
   });
 });
